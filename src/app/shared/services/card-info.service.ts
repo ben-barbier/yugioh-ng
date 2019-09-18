1e7da862bebde73bf8c379e2ea18a4a6b8cc27f1
Benjamin Barbier <benjamin.barbier.59@gmail.com>
2019-09-18 16:45:23 +0200

    :bug: fix api call size

--- src/app/shared/services/card-info.service.ts
+++ src/app/shared/services/card-info.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {CardInfo} from '../models/card-info.model';
 import {Observable} from 'rxjs';
+import {flatMap, take, toArray} from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
@@ -12,7 +13,11 @@ export class CardInfoService {
     }
 
     public getAll(): Observable<CardInfo[]> {
-        return this.http.get<CardInfo[]>('https://db.ygoprodeck.com/api/v5/cardinfo.php?format=Speed%20Duel');
+        return this.http.get<CardInfo[]>('https://db.ygoprodeck.com/api/v5/cardinfo.php?format=Speed%20Duel').pipe(
+            flatMap(e => e),
+            take(15),
+            toArray(),
+        );
     }
 
 }
